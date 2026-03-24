ff68ef6e8b0c932df8cacde51267b78a4f7271f5
allwayso <1784817625@qq.com>
2026-03-24 21:23:19 +0800

    feat: 优化游戏交互逻辑与图片资源配置
    - 限制游戏猜测次数为 2 次，结束后禁止再次提交
    - 统一 dishes.json 图片路径为本地测试路径
    - 优化 onSubmit 逻辑，增加游戏结束状态管理

--- miniprogram/pages/guess/guess.js
+++ miniprogram/pages/guess/guess.js
@@ -1,22 +1,49 @@
 Page({
   data: {
     dish: null,
+    restaurant: null,
     userPrice: '',
-    result: ''
+    result: '',
+    usedDishIds: [],
+    attempts: 0,
+    isGameOver: false
   },
   onLoad: function () {
     this.loadNextDish()
   },
   loadNextDish: function () {
-    this.setData({ dish: null, userPrice: '', result: '' })
+    this.setData({ dish: null, restaurant: null, userPrice: '', result: '', attempts: 0, isGameOver: false })
     const db = wx.cloud.database()
-    // 使用 aggregate 随机获取一条数据
-    db.collection('dishes').aggregate()
-      .sample({ size: 1 })
-      .end()
+    const _ = db.command
+    
+    db.collection('dishes')
+      .where({
+        _id: _.nin(this.data.usedDishIds)
+      })
+      .count()
       .then(res => {
-        if (res.list && res.list.length > 0) {
-          this.setData({ dish: res.list[0] })
+        if (res.total > 0) {
+          const skip = Math.floor(Math.random() * res.total)
+          db.collection('dishes')
+            .where({
+              _id: _.nin(this.data.usedDishIds)
+            })
+            .skip(skip)
+            .limit(1)
+            .get()
+            .then(res => {
+              if (res.data && res.data.length > 0) {
+                const dish = res.data[0]
+                this.setData({ 
+                  dish: dish,
+                  usedDishIds: [...this.data.usedDishIds, dish._id]
+                })
+                // 获取餐厅信息
+                db.collection('restaurants').doc(dish.restaurant_id).get().then(res => {
+                  this.setData({ restaurant: res.data })
+                })
+              }
+            })
         } else {
           this.setData({ result: '没有更多菜品了' })
         }
@@ -26,18 +53,39 @@ Page({
     this.setData({ userPrice: e.detail.value })
   },
   onSubmit: function () {
+    if (this.data.isGameOver) return
+
     const userPrice = parseInt(this.data.userPrice)
     const realPrice = this.data.dish.price
     if (isNaN(userPrice)) {
       this.setData({ result: '请输入有效的价格' })
       return
     }
+
+    let attempts = this.data.attempts + 1
+    const diff = Math.abs(userPrice - realPrice) / realPrice
+
+    let feedback = ''
+    let isGameOver = false
+
     if (userPrice === realPrice) {
-      this.setData({ result: '恭喜你，猜对了！' })
-    } else if (userPrice > realPrice) {
-      this.setData({ result: '猜高了，再试一次！' })
+      feedback = '神了！完全一致！'
+      isGameOver = true
+    } else if (diff <= 0.05) {
+      feedback = '太厉害了，简直是行家！'
+    } else if (diff <= 0.1) {
+      feedback = '眼光不错，非常接近了！'
+    } else if (diff <= 0.2) {
+      feedback = '有点感觉，再琢磨琢磨？'
     } else {
-      this.setData({ result: '猜低了，再试一次！' })
+      feedback = userPrice > realPrice ? '稍微有点高估了哦。' : '稍微有点低估了哦。'
     }
+
+    if (attempts >= 2 && !isGameOver) {
+      feedback += ` 游戏结束，真实价格是 ${realPrice} 元。`
+      isGameOver = true
+    }
+
+    this.setData({ result: feedback, attempts: attempts, isGameOver: isGameOver })
   }
 })
